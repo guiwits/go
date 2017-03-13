a54890290a61fa2783e3c2b7a7979e55ee82868f
Stephen Guiwits <guiwits@gmail.com>
2017-03-13 15:02:01 -0700

    initial commit of i3blocks weather program

---
+++ weather/README.md
@@ -0,0 +1,2 @@
+Just a quick program that gets the weather form http://api.openweatherm
+ap.org/data/2.5/weather to be used with i3blocks.
---
+++ weather/weather.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type Weather struct {
+	Id          int    `json:"id"`
+	Main        string `json:"main"`
+	Description string `json:"description"`
+	Icon        string `json:"icon"`
+}
+
+type WeatherData struct {
+	Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	}
+	Weather []Weather
+	Clouds  struct {
+		All int `json:"all"`
+	}
+	Main struct {
+		Temp     float64 `json:"temp"`
+		Pressure int     `json:"pressure"`
+		Humidity int     `json:"humidity"`
+		TempMin  float64 `json:"temp_min"`
+		TempMax  float64 `json:"temp_max"`
+	}
+	Sys struct {
+		Type    int     `json:"type"`
+		Id      int     `json:"id"`
+		Message float64 `json:"message"`
+		Country string  `json:"country"`
+		Sunrise int64   `json:"sunrise"`
+		Sunset  int64   `json:"sunset"`
+	}
+	Base       string `json:"base"`
+	Visibility int    `json:"visibility"`
+	Wind       struct {
+		Speed float64 `json:"speed"`
+		Deg   float64 `json:"deg"`
+		Gust  float64 `json:"gust"`
+	}
+	Dt   int64  `json:"dt"`
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+	Cod  int    `json:"cod"`
+}
+
+func main() {
+	// flags
+	var apiKey string
+	var cityId string
+	flag.StringVar(&apiKey, "key", "fffffffffffffffffffffff", "API Key from Openweather")
+	flag.StringVar(&cityId, "city", "5381396", "City ID fromOpenweather")
+	flag.Parse()
+
+	var weather WeatherData
+	var owmUrl string = "http://api.openweathermap.org/data/2.5/weather"
+	var units string = "imperial"
+	var weatherUrl string = owmUrl + "?id=" + cityId + "&appid=" + apiKey + "&units=" + units
+
+	var myClient = &http.Client{Timeout: 10 * time.Second}
+	// create reader to get URL request
+	//response, err := http.Get(weatherUrl) --> no timeout. bad.
+	response, err := myClient.Get(weatherUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	decodeErr := json.Unmarshal(body, &weather)
+	if decodeErr != nil {
+		fmt.Println("Error: ", decodeErr)
+	}
+	currentTemp := strconv.FormatFloat(weather.Main.Temp, 'f', -1, 64)
+	currentDesc := weather.Weather[0].Description
+	retVal := currentTemp + "°F : " + currentDesc
+	fmt.Print(retVal)
+}
+
+/*
+	fmt.Println(weather.Coord)
+	fmt.Println(weather.Weather)
+	fmt.Println(weather.Main)
+	fmt.Println(weather.Sys)
+	fmt.Println(weather.Base)
+	fmt.Println(weather.Clouds)
+	fmt.Println(weather.Visibility)
+	fmt.Println(weather.Wind)
+	fmt.Println(weather.Dt)
+	fmt.Println(weather.Id)
+	fmt.Println(weather.Name)
+	fmt.Println(weather.Cod)
+*/
